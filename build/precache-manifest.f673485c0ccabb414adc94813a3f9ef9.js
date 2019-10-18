c7628bfa928934a2ba4fd692db8503b550b43eb1
adaephonben <ma18btech11011@iith.ac.in>
2019-10-18 12:42:27 +0530

    Minor edits

---
+++ build/precache-manifest.f673485c0ccabb414adc94813a3f9ef9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe13502295ead76de04d98a118ed01d",
+    "url": "/portal/index.html"
+  },
+  {
+    "revision": "50e0e31b17d5b0474aef",
+    "url": "/portal/static/css/2.5b2e0181.chunk.css"
+  },
+  {
+    "revision": "a8eb17f5b39754ce496e",
+    "url": "/portal/static/css/main.da6556c6.chunk.css"
+  },
+  {
+    "revision": "50e0e31b17d5b0474aef",
+    "url": "/portal/static/js/2.c1d7b16c.chunk.js"
+  },
+  {
+    "revision": "a8eb17f5b39754ce496e",
+    "url": "/portal/static/js/main.328ba5bb.chunk.js"
+  },
+  {
+    "revision": "4e6d479ea49d564ca4f6",
+    "url": "/portal/static/js/runtime-main.a58c82b1.js"
+  },
+  {
+    "revision": "588ac60c0831f0585bfb01d218228bdc",
+    "url": "/portal/static/media/zozimus.588ac60c.png"
+  }
+]);
